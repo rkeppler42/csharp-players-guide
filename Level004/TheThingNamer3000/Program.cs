09bfb63ae024bd9d8da4a1efe73ebc0a8c25522a
Roberto Keppler <roberto.keppler@hotmail.com>
2025-02-07 23:56:51 -0300

    Ended Level004!

---
+++ Level004/TheThingNamer3000/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();  // Thing
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();  // Description
+string c = "of Doom";  // From
+string d = "3000";  // Version
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
+// We could be more verbose with our variables names!
